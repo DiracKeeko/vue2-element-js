235cabd75fa06b0f5d7eb91f0a1f8d2defec6b70
Keeko <787018661@qq.com>
2022-10-27 17:13:50 +0800

    [chore] vue2-element-js project init

---
+++ src/view/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <div class="home-page">HomePage</div>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <el-button type="primary" @click="emitMessage">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+  methods: {
+    async emitMessage() {
+      await this.$message("这是一条消息提示");
+      await this.$message({
+        message: "警告，这是一条警告消息",
+        type: "warning",
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.home-page {
+  margin: 200px 0 0 0;
+}
+</style>
