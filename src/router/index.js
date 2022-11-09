05ecf400abd8d0e331f88e30a0e6f3682b0a91da
keeko <787018661@qq.com>
2022-11-09 22:17:14 +0800

    [feat] format Json Case

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../view/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '@/view/About.vue')
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    component: () => import(/* webpackChunkName: "about" */ '@/view/TestJsonFormat.vue')
   },
   {
     path: '/*',
---
+++ src/view/TestJsonFormat.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="test-json-format">
+    <el-input type="textarea" :rows="4" placeholder="请输入JSON" v-model="textContent"> </el-input>
+    <el-button @click="handleJsonFormat">jsonFormat</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestJsonFormat',
+  data() {
+    return {
+      textContent: ''
+    };
+  },
+  mounted() {},
+  methods: {
+    trimJson(jsonStr) {
+      return jsonStr
+        .replace(/\n/g, '')
+        .replace(/\r/g, '')
+        .replace(/\s|\xA0/g, '');
+    },
+    handleJsonFormat() {
+      try {
+        const str = this.trimJson(this.textContent);
+        const jsonObj = JSON.parse(str);
+
+        if (typeof str === 'string' && typeof jsonObj === 'object' && obj) {
+          const jsonStr = JSON.stringify(jsonObj, null, 2);
+          this.textContent = jsonStr;
+        } else {
+          // this.errorHandler('输入有误, 请检查json输入');
+        }
+      } catch (err) {
+        console.log('err->', err);
+        // this.errorHandler('输入有误, 请检查json输入');
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.test-json-format {
+  width: 600px;
+  height: 400px;
+}
+</style>
