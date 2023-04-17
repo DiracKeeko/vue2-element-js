4c5cb5ca9624154dc1af3570fd51282038e4acca
keeko <787018661@qq.com>
2023-04-17 08:05:02 +0800

    [feat] 水平二级菜单+hover展示三级菜单03

--- src/view/showCase/showCusMultiMenu/ShowCusMultiMenu02.vue
+++ src/view/showCase/showCusMultiMenu/ShowCusMultiMenu02.vue
@@ -1,192 +1,90 @@
 <template>
-  <el-menu mode="horizontal" :default-active="activeIndex" class="navigation" :default-openeds="defaultOpeneds">
-    <template v-for="(menu, index) in menus">
-      <el-submenu v-if="menu.children && menu.children.length > 0" :key="index" :index="menu.path" :popper-class="['submenu-popper']" :show-timeout="0" :hide-timeout="0">
-        <template slot="title">{{ menu.name }}</template>
-        <el-row type="flex" class="submenu-row">
-          <el-col v-for="(child, idx) in menu.children" :key="idx">
-            <template v-if="child.children && child.children.length > 0">
-              <el-submenu :index="child.path" :popper-class="['submenu-popper']" :show-timeout="0" :hide-timeout="0">
-                <template slot="title">{{ child.name }}</template>
-                <el-menu-item v-for="(subchild, i) in child.children" :key="i" :index="subchild.path">{{ subchild.name }}</el-menu-item>
-              </el-submenu>
-            </template>
-            <template v-else>
-              <el-menu-item :index="child.path">{{ child.name }}</el-menu-item>
-            </template>
-          </el-col>
-        </el-row>
-      </el-submenu>
-      <el-menu-item v-else :key="index" :index="menu.path">{{ menu.name }}</el-menu-item>
-    </template>
+  <el-menu
+    class="nav-menu"
+    mode="horizontal"
+    @mouseenter="handleMenuEnter"
+    @mouseleave="handleMenuLeave"
+  >
+    <el-menu-item index="1">菜单1</el-menu-item>
+    <el-submenu index="2" ref="submenu2">
+      <template slot="title">
+        菜单2
+      </template>
+      <el-row class="submenu-row">
+        <el-col span="6">
+          <div>子菜单1</div>
+          <el-menu-item index="2-1-1">子菜单1-1</el-menu-item>
+          <el-menu-item index="2-1-2">子菜单1-2</el-menu-item>
+          <el-menu-item index="2-1-3">子菜单1-3</el-menu-item>
+        </el-col>
+        <el-col span="6">
+          <div>子菜单2</div>
+          <el-menu-item index="2-2-1">子菜单2-1</el-menu-item>
+          <el-menu-item index="2-2-2">子菜单2-2</el-menu-item>
+          <el-menu-item index="2-2-3">子菜单2-3</el-menu-item>
+        </el-col>
+        <el-col span="6">
+          <div>子菜单3</div>
+          <el-menu-item index="2-3-1">子菜单3-1</el-menu-item>
+          <el-menu-item index="2-3-2">子菜单3-2</el-menu-item>
+          <el-menu-item index="2-3-3">子菜单3-3</el-menu-item>
+        </el-col>
+      </el-row>
+    </el-submenu>
+    <el-menu-item index="3">菜单3</el-menu-item>
   </el-menu>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      activeIndex: '', // 当前选中的菜单项
-      defaultOpeneds: ['/menu1/menu1-1'], // 默认展开的菜单项的路径数组
-      menus: [ // 导航菜单数据
-        {
-          name: '一级菜单1',
-          path: '/menu1',
-          children: [
-            {
-              name: '二级菜单1-1',
-              path: '/menu1/menu1-1',
-              children: [
-                {
-                  name: '三级菜单1-1-1',
-                  path: '/menu1/menu1-1/menu1-1-1'
-                },
-                {
-                  name: '三级菜单1-1-2',
-                  path: '/menu1/menu1-1/menu1-1-2'
-                }
-              ]
-            },
-            {
-              name: '二级菜单1-2',
-              path: '/menu1/menu1-2'
-            }
-          ]
-        },
-        {
-          name: '一级菜单2',
-          path: '/menu2'
-        },
-        {
-          name: '一级菜单2',
-          path: '/menu2'
-        }
-      ]
-    }
-  }
-}
+  methods: {
+    handleMenuEnter() {
+      this.$refs.submenu2.forEach((submenu) => {
+        submenu.showPopper();
+        submenu.$refs.submenu.forEach((submenu) => {
+          submenu.showPopper();
+        });
+      });
+    },
+    handleMenuLeave() {
+      this.$refs.submenu2.forEach((submenu) => {
+        submenu.hidePopper();
+        submenu.$refs.submenu.forEach((submenu) => {
+          submenu.hidePopper();
+        });
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-.navigation {
-  background-color: #fff;
-  border-bottom: 1px solid #e6e6e6;
+.el-menu--horizontal .el-submenu__title:hover .el-icon-arrow-down,
+.el-menu--horizontal .el-submenu__title:hover .el-icon-arrow-right {
+  transform: rotate(180deg);
 }
 
-.submenu-popper {
-  top: 0 !important;
-  left: 100% !important;
-  margin-top: -1px;
+.el-menu--horizontal .el-submenu__title .el-icon-arrow-down,
+.el-menu--horizontal .el-submenu__title .el-icon-arrow-right {
+  transition: all 0.3s;
+  margin-left: 5px;
 }
 
-.el-submenu__title:hover {
-  background-color: #f5f5f5;
-}
-
-.submenu-row {
-  min-width: 200px;
-  margin-top: -1px;
-}
-
-.el-menu-item {
-  padding-left: 20px !important;
-}
-
-.el-menu--horizontal .el-submenu__title {
-  margin-bottom: 0;
-}
-
-.el-menu--horizontal .el-submenu__title:hover {
-  background-color: #f5f5f5;
-}
-
-.el-menu--horizontal .el-submenu__title:hover .el-icon-arrow-down {
-  color: #409EFF;
-}
-
-.el-menu--horizontal .el-submenu__title .el-icon-arrow-down {
-  color: #c0c4cc;
-}
-
-.el-menu--horizontal .el-submenu__title .el-icon-arrow-down:hover {
-  color: #409EFF;
-}
-
-.el-menu-item.is-active {
-  color: #409EFF !important;
-  background-color: #f5f5f5 !important;
-}
-
-.el-menu-item.is-active:hover {
-  background-color: #f5f5f5 !important;
-}
-
-/* 新增样式 */
-.el-submenu__popper .el-menu {
-  width: auto;
-  background-color: #f5f5f5;
-  box-shadow: none;
-  border-bottom: none;
-}
-
-.el-submenu__popper .el-submenu__title {
-  padding: 0;
-}
-
-.el-submenu__popper .el-submenu__title:hover {
-  background-color: transparent;
-}
-
-.el-submenu__popper .el-submenu__title .el-icon-arrow-right {
-  color: #c0c4cc;
-  margin-left: 0;
-  margin-right: 5px;
-}
-
-.el-submenu__popper .el-submenu__title .el-icon-arrow-down {
-  color: #409EFF;
-  margin-left: 0;
-  margin-right: 5px;
-}
-
-.el-submenu__popper .el-submenu__title .el-icon-arrow-right:hover {
-  color: #c0c4cc;
-}
-
-.el-submenu__popper .el-submenu__title .el-icon-arrow-down:hover {
-  color: #409EFF;
-}
-
-.el-submenu__popper .el-menu-item {
-  padding-left: 20px !important;
-}
-
-.el-submenu__popper .el-menu-item:hover {
-  background-color: #f5f5f5 !important;
-  color: #409EFF !important;
-}
-
-.el-submenu__popper .el-menu-item.is-active {
-  background-color: #f5f5f5 !important;
-  color: #409EFF !important;
-}
-
-.el-submenu__popper .el-menu-item.is-active:hover {
-  background-color: #f5f5f5 !important;
-  color: #409EFF !important;
-}
-
-.el-submenu__popper .el-submenu__wrapper {
-  margin: 0;
-  border: none;
-}
-
-.el-submenu__popper .el-menu--horizontal .el-submenu__title {
-  margin-bottom: 0;
-}
-
-.el-submenu__popper .el-row {
+.el-row {
   display: flex;
   flex-wrap: wrap;
 }
-</style>
+
+.el-col {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.el-menu-item.is-active,
+.el-submenu.is-active > .el-submenu__title {
+  background-color: #f5f5f5;
+  color: #409EFF;
+}
+
