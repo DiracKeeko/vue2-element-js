c8ba70a10678a53d6923632f1a9f2e76594294d7
Keeko <787018661@qq.com>
2023-05-17 20:47:27 +0800

    [feat] thePlainTable补充表格列排序方法(无法获知asc desc)

--- src/component/ThePlainTable.vue
+++ src/component/ThePlainTable.vue
@@ -1,33 +1,22 @@
 <template>
-  <el-table
-    v-bind="$attrs"
-    :data="tableData"
-    row-key="name"
-    style="width: 100%"
-  >
+  <el-table v-bind="$attrs" :data="tableData" row-key="name" style="width: 100%">
     <el-table-column
       v-for="col in tableTitle"
-      :key="col.key||col.prop"
+      :key="col.key || col.prop"
       v-bind="col"
+      :sort-method="
+        col.customSortMethod
+          ? (a, b) => sortMethodObj[col.customSortMethod](a, b, col.prop)
+          : undefined
+      "
       :show-overflow-tooltip="!col['show-overflow-tooltip']"
     >
       <template v-if="col.slotHeader" #header="{ column, $index }">
-        <slot
-          :name="col.slotHeader"
-          :row="column"
-          :index="$index"
-        >
-        </slot>
+        <slot :name="col.slotHeader" :row="column" :index="$index"> </slot>
       </template>
 
       <template v-slot="{ row, $index }">
-        <slot
-          v-if="col.slotName"
-          :name="col.slotName"
-          :row="row"
-          :index="$index"
-        >
-        </slot>
+        <slot v-if="col.slotName" :name="col.slotName" :row="row" :index="$index"> </slot>
         <span v-else>{{ row[col.prop] }}</span>
       </template>
     </el-table-column>
@@ -41,21 +30,36 @@ export default {
       type: Array,
       default() {
         return [];
-      },
+      }
     },
     tableTitle: {
       type: Array,
       default() {
         return [];
-      },
+      }
     },
     parentFormatter: {
       type: Function,
-      default() {},
-    },
+      default() {}
+    }
   },
   data() {
-    return {};
+    return {
+      sortMethodObj: {
+        nullToTail(a, b, prop) {
+          if (a[prop] === undefined) {
+            return 1;
+          }
+          if (b[prop] === undefined) {
+            return -1;
+          }
+          if (isNaN(Number(a[prop]))) {
+            return a[prop].localeCompare(b[prop]);
+          }
+          return a[prop] - b[prop];
+        }
+      }
+    };
   },
   methods: {
     basicTableCellFormatter() {
@@ -67,7 +71,7 @@ export default {
       }
       if (!value) return "--";
       return value;
-    },
-  },
+    }
+  }
 };
 </script>
