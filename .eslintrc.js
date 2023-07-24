aa6d5d45e7e6233762c9d075ef1e02447bf7c1e8
Keeko <787018661@qq.com>
2023-07-24 10:43:15 +0800

    [feat] husky in project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,24 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    browser: true,
+    es6: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
+    "airbnb-base",
+    "eslint:recommended",
+    "plugin:vue/essential",
+    "prettier",
+    "prettier/vue"
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
     // 'vue/no-v-for-template-key': 'off', // vue2
     // 'vue/no-v-for-template-key-on-child': 'off' // vue3
   }
-}
+};
